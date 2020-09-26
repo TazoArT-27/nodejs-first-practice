6b772e63654e7dd61d5b75d7e1717ef2dc8fb538
1810050 <67385245+1810050@users.noreply.github.com>
2020-09-27 00:50:21 +0600

    node learning primary done

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 const express = require('express')
-
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
 const app = express()
 
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const users = ['shafayat', 'tazoar', 'afi', 'sadat']
+
 app.get('/', (req, res) => {
-    res.send('Thank you for calling me')
+    const fruit = {
+        product : "mango",
+        price: 200
+    }
+    res.send(fruit);
 })
 
-app.listen(3000, () => console.log('listening port 3000'))
+app.get('/fruits/banana', (req, res) => {
+    res.send({fruit:'banana', price: 200});
+})
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const name = users[id];
+    res.send({id, name});
+    // console.log(req.params);
+})
+
+//post
+app.post('/addUser', (req, res) => {
+    //save to database
+    const user = req.body;
+    user.id = 55;
+    res.send(user)
+    //console.log('post request send')
+})
+
+app.listen(4200, () => console.log('listening port 4200'))
